cb77ebe8e9bb1f0d264236bd62ef559301197808
Emil <emilsandgren94@hotmail.com>
2015-11-08 16:29:47 +0100

    fixat

--- src/snake/SnakeUI.java
+++ src/snake/SnakeUI.java
@@ -1,16 +1,12 @@
 package snake;
 
-import com.sun.glass.ui.Size;
-import com.sun.prism.paint.*;
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.Color;
-import java.lang.reflect.Array;
-import java.util.ArrayList;
 
 /**
  * Created by Emil on 2015-11-07.
+ * Klass som visar algoritmen grafiskt.
  */
 public class SnakeUI extends JPanel {
     private JPanel pnlCenter = new JPanel();
@@ -18,14 +14,13 @@ public class SnakeUI extends JPanel {
     private JTextPane textPane = new JTextPane();
     private GridLayout gridLayout;
 
-
     public SnakeUI(Brick[][] brickArray) {
-
         setLayout(new BorderLayout());
         pnlCenter.setPreferredSize(new Dimension(800,600));
         add(pnlCenter, BorderLayout.CENTER);
         add(pnlSouth,BorderLayout.SOUTH);
         pnlSouth.setBackground(Color.WHITE);
+
         JFrame frame = new JFrame();
         frame.add(this);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
@@ -33,26 +28,28 @@ public class SnakeUI extends JPanel {
         frame.setSize(new Dimension(600, 400));
         Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
         frame.setLocation(dim.width/2-frame.getSize().width/2, dim.height/2-frame.getSize().height/2);
-
-
-
     }
 
-
+    /**
+     * Tar in den största vägen och skriver in det i en panel
+     * @param size storleken på största vägen
+     */
     public void setPnlSouth(int size) {
         pnlSouth.add(textPane);
         textPane.setText("Longest path it could find is "+size+" steps");
     }
 
+    /**
+     * Bygger uit genom att ta in brickarrayen och sätter olika färger
+     * beroende på vilken egenskap varje brick har.
+     * @param brickArray färdiga "snabbaste vägen" arrayen
+     */
     public void buildUI(Brick[][] brickArray) {
-
         pnlCenter.removeAll();
-
         int nbrColums = brickArray.length;
         int nbrRows = brickArray[0].length;
         gridLayout = new GridLayout(nbrColums,nbrRows);
         pnlCenter.setLayout(gridLayout);
-
 
         for (int i = 0; i < brickArray.length; i++) {
             for (int j = 0; j < brickArray[i].length; j++) {
@@ -61,19 +58,21 @@ public class SnakeUI extends JPanel {
                     JButton btnOb = new JButton();
                     btnOb.setBackground(Color.RED);
                     pnlCenter.add(btnOb);
+
                 } else if (brickArray[i][j].isVisited()) {
                     Color color = new Color(0, (254 / ((brickArray[i][j].getOrder() + 1)/2 +1)), 0);
                     JButton btnVis = new JButton();
                     btnVis.setBackground(color);
-                    if(((brickArray[i][j].getOrder()==0))) {
+
+                    if (((brickArray[i][j].getOrder()==0))) {
                         btnVis.setForeground(Color.DARK_GRAY);
+
                     } else {
                         btnVis.setForeground(Color.WHITE);
                     }
-
                     btnVis.setText(brickArray[i][j].getOrder() + "");
-
                     pnlCenter.add(btnVis);
+
                 } else {
                     JButton btn = new JButton();
                     btn.setBackground(Color.LIGHT_GRAY);
@@ -82,9 +81,6 @@ public class SnakeUI extends JPanel {
                 }
             }
         }
-
         pnlCenter.validate();
-
     }
-
 }
